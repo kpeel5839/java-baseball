61d209179a926b09945fab3495e5d3e89671863a
jaeyeon kim <jakind@naver.com>
2022-11-25 16:31:23 +0900

    feat(NumberValidator): 해당 문자열이 숫자로만 이루어져있는지 확인하는 Validator 추가

---
+++ src/main/java/baseball/validator/NumberValidator.java
@@ -0,0 +1,19 @@
+package baseball.validator;
+
+public class NumberValidator {
+    
+    // 여기서는 Number 인지만 확인하면 된다.
+    public void validate(String input) {
+        for (int index = 0; index < input.length(); index++) {
+            char characterOfNowIndex = input.charAt(index);
+
+            if (isNotNumber(characterOfNowIndex)) {
+                throw new IllegalArgumentException(); // 나중에 Exception Message 채워넣자
+            }
+        }
+    }
+
+    private boolean isNotNumber(char character) {
+        return !('1' <= character && character <= '9');
+    }
+}
