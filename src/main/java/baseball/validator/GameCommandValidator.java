4fef70b60b96d795cff8b15082f1d987229caa9d
jaeyeon kim <jakind@naver.com>
2022-11-25 16:44:15 +0900

    feat(GameCommandValidator): 재시작 or 종료 입력을 검증하는 기능 추가

--- src/main/java/baseball/validator/GameCommandValidator.java
+++ src/main/java/baseball/validator/GameCommandValidator.java
@@ -1,4 +1,28 @@
 package baseball.validator;
 
 public class GameCommandValidator {
+
+    private final int GAME_COMMAND_SIZE = 1;
+
+    private final NumberValidator numberValidator;
+
+    public GameCommandValidator() {
+        numberValidator = new NumberValidator();
+    }
+
+    public void validate(String input) {
+        numberValidator.validate(input);
+
+        if (isNotOneDigit(input) || isNotOneOrTwo(input)) {
+            throw new IllegalArgumentException(); // Add Exception Message Later
+        }
+    }
+
+    private boolean isNotOneDigit(String input) {
+        return input.length() != GAME_COMMAND_SIZE;
+    }
+
+    private boolean isNotOneOrTwo(String input) {
+        return !(input.equals("1") || input.equals("2"));
+    }
 }
