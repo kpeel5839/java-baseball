7a781fd80ef025283108345db5f1cd783409966f
jaeyeon kim <jakind@naver.com>
2022-11-25 16:46:23 +0900

    feat(validator): Balls, GameCommand 입력을 예외 처리하는 기능 추가

--- src/main/java/baseball/validator/Validator.java
+++ src/main/java/baseball/validator/Validator.java
@@ -1,4 +1,20 @@
 package baseball.validator;
 
 public class Validator {
+
+    private final BallsValidator ballsValidator;
+    private final GameCommandValidator gameCommandValidator;
+
+    public Validator() {
+        ballsValidator = new BallsValidator();
+        gameCommandValidator = new GameCommandValidator();
+    }
+
+    public void ballsValidate(String input) {
+        ballsValidator.validate(input);
+    }
+
+    public void gameCommandValidate(String input) {
+        gameCommandValidator.validate(input);
+    }
 }
