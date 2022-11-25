06c85a07d7f50ec1a27bbefddc1da25de22338a7
jaeyeon kim <jakind@naver.com>
2022-11-25 16:51:22 +0900

    test(ValidatorTest): BallsValidate 메소드 검증

---
+++ src/test/java/baseball/validator/ValidatorTest.java
@@ -0,0 +1,34 @@
+package baseball.validator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ValidatorTest {
+
+    private Validator validator;
+
+    @BeforeEach
+    void setUp() {
+        validator = new Validator();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"123", "456"})
+    void inputCorrectBalls(String input) {
+        // then
+        validator.ballsValidate(input); // 예외 발생 x
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"112", "334", "A", "12a"})
+    void inputWrongBalls(String input) {
+        assertThatThrownBy(() -> validator.ballsValidate(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    
+}
